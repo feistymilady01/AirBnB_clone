fbc8645d361be673705748d56ee0fbf140a49e8f
susan <pricelessblinks@gmail.com>
2023-02-08 12:43:01 +0000

    work

---
+++ models/base_model.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This script is the base model"""
+
+import uuid
+from datetime import datetime
+from models import storage
---
+++ web_static/W3C-Validator/w3c_validator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+W3C validator for Holberton School
+For HTML and CSS files.
+Based on 2 APIs:
+    - https://validator.w3.org/nu/
+    - http://jigsaw.w3.org/css-validator/validator
+
