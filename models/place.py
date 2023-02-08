71590e6d11a4554b061a373bcec5bb151987c1ba
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-02-08 14:02:11 +0100

    place

--- models/place.py
+++ models/place.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-"""This module creates a Review class"""
+"""This module creates a Place class"""
 
 from models.base_model import BaseModel
 
 
-class Review(BaseModel):
-        """Class for managing review objects"""
+class Place(BaseModel):
+    """Class for managing place objects"""
 
-            place_id = ""
-                user_id = ""
-                    text = ""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
